7048adf3568824b719020f761f14c503a8e35824
sergej-potapov-pb <sergej.potapov@privatbank.ua>
2022-01-15 01:39:21 +0200

    task 2

---
+++ lesson-02/task-1.js
@@ -0,0 +1,9 @@
+//if (a + b < 4) {
+//result = true;
+//} else {
+//result = false;
+//}
+
+var a = 2;
+var b = 2;
+let result = (a+b <4) ? true : false;
---
+++ lesson-02/task-2.js
@@ -0,0 +1,15 @@
+// var message;
+//
+// if (login == 'Pitter') {
+// message = 'Hi';
+// } else if (login == 'Owner') {
+// message = 'Hello';
+// } else if (login == '') {
+// message = 'unknown';
+// } else {
+// message = '';
+// }
+
+let message = (login == 'Pitter') ? 'Hi':
+              (login == 'Owner') ? 'Hello':
+              (login == '') ? 'unknown':'';
