f2f9573a9d8c9c5195a2db271149e522420c9006
sergej-potapov-pb <sergej.potapov@privatbank.ua>
2022-01-21 02:39:59 +0200

    lesson 3

--- lesson-03/task-4.js
+++ lesson-03/task-4.js
@@ -9,3 +9,33 @@ f('1'); // Error: parameter type is not a Number
 ```
 */
 
+function f(a){
+    let res;
+    if( typeof a==='number'){
+        switch(a){
+            case 1: res ="понедельник";
+                    break;
+            case 2: res = "вторник";        
+                    break;
+            case 3: res = "среда";        
+                    break;
+            case 4: res = "четверг";        
+                    break;
+            case 5: res = "пятница";        
+                    break;
+            case 6: res = "суббота";        
+                    break;
+            case 7: res = "восскресение";        
+                    break;
+     
+        }
+    } else {
+        throw new Error("parameter type is not a Number");
+    }
+    return res;
+}
+
+console.log( f(1)); // Воскресенье
+console.log( f(2); // Понедельник
+console.log( f(8); // Error: parameter should be in the range of 1 to 7
+console.log( f('1'); // Error: parameter type is not a Number
