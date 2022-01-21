b8e6603dcd138041def8873beda5512abf8c632a
sergej-potapov-pb <sergej.potapov@privatbank.ua>
2022-01-21 12:35:54 +0200

    lesson 3

--- lesson-03/task-7.js
+++ lesson-03/task-7.js
@@ -9,3 +9,25 @@ getDivisors(0); // Error: parameter can't be a 0
 
 */
 
+function getDivisors(numb) {
+  var resArr = [];
+  if (typeof numb === "number") {
+    if (numb != 0) {
+      for (var i = 1; i <= numb; i++) {
+        if (numb % i == 0) {
+          resArr.push(i);
+        }
+      }
+    } else {
+      throw new Error("Error: parameter can't be a 0");
+    }
+  } else {
+    throw new Error("Error: parameter type is not a Number");
+  }
+  return resArr;
+}
+
+console.log(getDivisors(12)); // [1, 2, 3, 4, 6, 12]
+console.log(getDivisors(28));
+console.log(getDivisors("Content")); // Error: parameter type is not a Number
+//console.log(getDivisors(0)); // Error: parameter can't be a 0
