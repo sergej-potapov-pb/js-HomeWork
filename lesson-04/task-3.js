27cb9d596f5f0f468ff7d4562fbf672c1aff9fa0
sergej-potapov-pb <sergej.potapov@privatbank.ua>
2022-01-28 11:39:21 +0200

    lesson 4

--- lesson-04/task-3.js
+++ lesson-04/task-3.js
@@ -1,15 +1,4 @@
 /* 
-
-Classroom LogoКласс
-Здравствуйте, Сергей.
-Преподаватель Антон Олегович Подгорный опубликовал новое задание для курса "JS online".
-			
-	
-Новое задание
-	Сдать 28 янв.
-Задача-3
-####Задача 3
-
 Создать имплементацию функции `every`, логика работы такая же как и у родного метода.
 
 Функция должна содержать проверки:
@@ -22,3 +11,25 @@ const arr = [1,2,3];
 every(arr, function(item, i, arr) {});
 ```
 */
+
+const arrSrc = [3, 3, 3, 3, 3, 3];
+
+let myEvery = function (arr, callback) {
+  if (Array.isArray(arr) && typeof callback === "function") {
+    let length = arr.length;
+    for (let i = 0; i < length; i++) {
+      if (!callback.call(i, arr[i])) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    throw new Error("не верно заданы параметры функции myFilter");
+  }
+};
+
+let resEvery = myEvery(arrSrc, function (num) {
+  return num === 3;
+});
+
+console.log(resEvery);
