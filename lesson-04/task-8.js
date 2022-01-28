93434041435c22df130045a63a10c76eade35a80
sergej-potapov-pb <sergej.potapov@privatbank.ua>
2022-01-29 01:06:11 +0200

    lesson 4

--- lesson-04/task-8.js
+++ lesson-04/task-8.js
@@ -20,6 +20,52 @@ f(arr4); // 0
 const arr5 = [[[[[],3]]]];
 f(arr5); // 3
 ```
-
-
 */
+
+function f(arr) {
+  if (Array.isArray(arr)) {
+    let resArr = arr.flat(Infinity);
+    //console.log(resArr);
+
+    let result = 0;
+    let length = resArr.length;
+
+    for (let i = 0; i < length; i++) {
+      if (typeof resArr[i] === "number") {
+        result += resArr[i];
+      } else {
+        throw new Error("элемент массива должен быть числом");
+      }
+    }
+    return result;
+  } else {
+    throw new Error("не верно заданы параметры функции");
+  }
+}
+
+const arr = [
+  [
+    [1, 2],
+    [1, 2],
+  ],
+  [
+    [2, 1],
+    [1, 2],
+  ],
+];
+console.log(f(arr));
+
+const arr2 = [[[[[1, 2]]]]];
+console.log(f(arr2));
+
+const arr3 = [[[[[1, 2]]], 2], 1];
+console.log(f(arr3));
+
+const arr4 = [[[[[]]]]];
+console.log(f(arr4));
+
+const arr5 = [[[[[], 3]]]];
+console.log(f(arr5));
+
+const arr6 = [1, 2, 3, "d", 5, 6, 7];
+console.log(f(arr6));
