5c83cfa808a4291a71303af22034da373ac549a0
sergej-potapov-pb <sergej.potapov@privatbank.ua>
2022-01-28 14:18:41 +0200

    lesson 4

--- lesson-04/task-5.js
+++ lesson-04/task-5.js
@@ -13,3 +13,48 @@ const acc = 0;
 reduce(arr, function(acc, item, i, arr) {}, acc);
 ```
 */
+
+const arrSrc = [1, 2, 5, 6, 3, 10];
+//const arrSrc = [2, 5, 2];
+const nsStart = 0;
+
+let myReduce = function (arr, callback, ns) {
+  let result = ns;
+  if (
+    Array.isArray(arr) &&
+    typeof callback === "function" &&
+    (typeof ns === "string" || typeof ns === "number")
+  ) {
+    let length = arr.length;
+    for (let i = 0; i < length; i++) {
+      result = callback.call(null, result, arr[i]);
+    }
+  } else {
+    throw new Error("не верно заданы параметры функции myFilter");
+  }
+  return result;
+};
+
+let resReduce = myReduce(
+  arrSrc,
+  function (nsStart, num) {
+    return nsStart + num;
+  },
+  nsStart
+);
+
+console.log(resReduce);
+console.log("-------");
+
+const arrStr = ["Проверяем ", "мою ", "функцию ", "Reduce"];
+const startStr = "";
+
+console.log(
+  myReduce(
+    arrStr,
+    function (startStr, word) {
+      return startStr.concat(word);
+    },
+    startStr
+  )
+);
