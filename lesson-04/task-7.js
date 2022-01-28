8c96de5b3f7a111d9b7324d0743964f562f7c51f
sergej-potapov-pb <sergej.potapov@privatbank.ua>
2022-01-28 17:09:20 +0200

    lesson 4

--- lesson-04/task-7.js
+++ lesson-04/task-7.js
@@ -16,3 +16,18 @@ console.log( arrLength ); // 4,5,2,3
 
 - используйте вместо цикла `for` метод `map`
 */
+
+var arr = ["abcd", "abcde", "ab", "abc"];
+
+var arrLength = [];
+for (var i = 0; i < arr.length; i++) {
+  arrLength[i] = arr[i].length;
+}
+
+console.log(arrLength); // 4,5,2,3
+console.log("-------");
+
+arrLength = arr.map(function (el) {
+  return el.length;
+});
+console.log(arrLength); // 4,5,2,3
