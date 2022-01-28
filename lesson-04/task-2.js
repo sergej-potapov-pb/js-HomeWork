d39d1f944de9aecba88782f81f11f91ffe5abb85
sergej-potapov-pb <sergej.potapov@privatbank.ua>
2022-01-28 10:59:59 +0200

    lesson 4

--- lesson-04/task-2.js
+++ lesson-04/task-2.js
@@ -11,3 +11,26 @@ const arr = [1,2,3];
 filter(arr, function(item, i, arr) {});
 ```
 */
+
+const arrSrc = [1, 2, 5, 6, 3, 10];
+
+let myFilter = function (arr, callback) {
+  let resFilter = [];
+  if (Array.isArray(arr) && typeof callback === "function") {
+    let length = arr.length;
+    for (let i = 0; i < length; i++) {
+      if (callback.call(i, arr[i])) {
+        resFilter.push(arr[i]);
+      }
+    }
+    return resFilter;
+  } else {
+    throw new Error("не верно заданы параметры функции myFilter");
+  }
+};
+
+let resArr = myFilter(arrSrc, function (num) {
+  return num > 3;
+});
+
+console.log(resArr);
